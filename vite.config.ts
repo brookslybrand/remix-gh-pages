0e3b7959de70644038a8f7b9e1de2a18d25c7f21
Sergio Xalambrí <hello@sergiodxa.com>
2024-03-12 17:21:22 -0500

    Use Vite plugin buildEnd hook to copy index.html to 404.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 export default defineConfig({
   base: "/remix-gh-pages/",
@@ -8,6 +10,16 @@ export default defineConfig({
     remix({
       ssr: false,
       basename: "/remix-gh-pages/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+
+        // copy the index.html to 404.html for GH Pages
+        let buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html"),
+        );
+      },
     }),
     tsconfigPaths(),
   ],
