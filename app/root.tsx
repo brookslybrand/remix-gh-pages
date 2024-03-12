6c49bd9110a39822c8a5e51d3ac0b8e9ccb070dc
Brooks Lybrand <brookslybrand@gmail.com>
2024-03-12 10:45:24 -0500

    Can I get a little favicon?

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
         <Meta />
         <Links />
       </head>
