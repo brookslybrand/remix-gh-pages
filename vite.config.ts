eea8f1b29d271e8961a96a5b4d5c090469dc4c11
Brooks Lybrand <brookslybrand@gmail.com>
2024-03-12 09:40:22 -0500

    Add that basename!

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/remix-gh-pages",
   plugins: [
     remix({
       ssr: false,
