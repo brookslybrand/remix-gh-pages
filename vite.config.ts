c1bb399f866acad787810d70f75aa9183585ab87
Sergio Xalambrí <hello@sergiodxa.com>
2024-03-12 17:25:04 -0500

    Add comment explaining why it's needed

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,14 @@ export default defineConfig({
       buildEnd(args) {
         if (!args.viteConfig.isProduction) return;
 
-        // copy the index.html to 404.html for GH Pages
+        // When deploying to GitHub Pages, if you navigate from / to another
+        // route and refresh the tab, it will show the default GH Pages 404
+        // page. This happens because GH Pages is not configured to send all
+        // traffic to the index.html where we can load our client-side JS.
+        // To fix this, we can create a 404.html file that contains the same
+        // content as index.html. This way, when the user refreshes the page,
+        // GH Pages will serve our 404.html and everything will work as
+        //expected.
         const buildPath = args.viteConfig.build.outDir;
         copyFileSync(
           join(buildPath, "index.html"),
